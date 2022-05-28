ec40565bd8da84f49a17759a23a3f1e79d4ef43c
igojig <igojig@mail.ru>
2022-05-28 16:40:10 +0300

    Initial commit

---
+++ src/main/java/life/Config.java
@@ -0,0 +1,18 @@
+package life;
+
+public interface Config {
+    int SIZE = 5;
+    int X_S = 200;
+    int Y_S = 200;
+
+    int RND = (int) (X_S * Y_S * 0.5);
+//    static int RND = 150;
+
+    int DELAY = 5;
+
+    boolean RULE = false;
+
+    int MAX_ELEMENTS=30;
+
+
+}
---
+++ src/main/java/life/Main.java
@@ -0,0 +1,19 @@
+package life;
+
+
+public class Main {
+    public static void main(String[] args)  {
+
+        Life life=new Life(Config.X_S, Config.Y_S, Config.RND);
+        new Window(life);
+
+//        OptionalInt optionalInt=OptionalInt.empty();
+//        List<Point2D.Double> list=ValtrAlgorithm.generateRandomConvexPolygon(10);
+
+//        Func.divide(2, 2);
+
+    }
+}
+
+
+
